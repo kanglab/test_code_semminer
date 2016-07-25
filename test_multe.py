b19895f935fb592732a7588dc855ee76e76bb33a
U-KANAZUCHI-PC\hammer <mamerh8823@yahoo.co.jp>
2016-07-25 17:33:03 +0900

    テスト成功

--- test_multe.py
+++ test_multe.py
@@ -3,4 +3,11 @@ import multe
 
 class TestMulte(unittest.TestCase):
 	def test_multe_2_values(self):
-self.assertEqual(multe.multe_2_values(3, 5), 15)
+		self.assertEqual(multe.multe_2_values(3, 5), 15)
+
+def main():
+	unittest.main()
+
+
+if __name__ == '__main__':
+	main()
