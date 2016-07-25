10df1aa110e9b14dec0a4adbbec451d9167a91a0
nakagawa <0419ikkan@gmail.com>
2016-07-25 17:30:09 +0900

    test

--- test_sub.py
+++ test_sub.py
@@ -2,5 +2,11 @@ import unittest
 import sub
 
 class TestSubtract(unittest.TestCase):
-	def test_subtract_2_values(self):
-		self.assertEqual(sub.subtract_2_values(5, 3), 2)
+	def test_substract_2_values(self):
+		self.assertEqual(sub.substract_2_values(5, 3), 2)
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
